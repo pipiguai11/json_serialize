4ec6888cfb66a643f5b0f8f99dccc7849944e259
林汉文 <linhw@dist.com.cn>
2021-01-11 16:03:49 +0800

    fix:修改文件加载方式，直接从templates目录下读取 原先是从本地磁盘读取文件

--- src/test/java/com/lhw/serialize/JsonSerializeApplicationTests.java
+++ src/test/java/com/lhw/serialize/JsonSerializeApplicationTests.java
@@ -2,12 +2,36 @@ package com.lhw.serialize;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @SpringBootTest
 class JsonSerializeApplicationTests {
 
+    public static final String JSON_DATA_PATH = "templates/file/jsonData.txt";
+
+    /**
+     * 从template/file目录中读取文件并输出
+     */
     @Test
     void contextLoads() {
+        try {
+            InputStream in = new ClassPathResource(JSON_DATA_PATH).getInputStream();
+            byte[] b = new byte[4];
+            StringBuffer sb = new StringBuffer();
+            int len = -1;
+            while ((len = in.read(b)) != -1){
+                sb.append(new String(b, StandardCharsets.UTF_8));
+            }
+            System.out.println(sb);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
     }
 
 }
